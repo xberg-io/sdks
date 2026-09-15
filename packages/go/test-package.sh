61c0bb814c77aed28ea1e476400b5916166d1863
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-09-15 19:06:03 +0200

    style(shell): format test-package.sh
    Pre-existing drift: shell formatting was enabled repo-wide in 7582a48 without
    reformatting this file, so `task format:check` has been failing on it since.

--- packages/go/test-package.sh
+++ packages/go/test-package.sh
@@ -6,13 +6,13 @@ trap 'rm -rf "$package_dir"' EXIT
 mkdir "$package_dir/sdk" "$package_dir/consumer"
 git -C "$root" archive HEAD packages/go | tar -x -C "$package_dir/sdk" --strip-components=2
 for generated in generated_api.go generated_pro.go generated_backend.go; do
-  test -s "$package_dir/sdk/$generated"
+	test -s "$package_dir/sdk/$generated"
 done
 cd "$package_dir/consumer"
 export GOWORK=off
 go mod init consumer.example/sdk
 go mod edit -replace "github.com/xberg-io/sdks/packages/go=$package_dir/sdk"
-cat > main.go <<'GO'
+cat >main.go <<'GO'
 package main
 
 import xberg "github.com/xberg-io/sdks/packages/go"
